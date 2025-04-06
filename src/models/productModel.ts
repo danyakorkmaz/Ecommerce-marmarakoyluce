9648b8f892d041de79d7e05e9a3545bf74b1ae38
danyakorkmaz <106125107+danyakorkmaz@users.noreply.github.com>
2025-04-06 15:21:20 +0300

    Modified product model attributes

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,15 +1,15 @@
 import mongoose, { Schema, Document, ObjectId } from "mongoose";
 
 export interface IProduct extends Document {
-    title: string; 
-    description: string; 
-    image: string; 
-    otherImages: string[]; 
+    title: string;
+    description: string;
+    image: string;
+    otherImages: string[];
     SKU: string; // Stok kodu (Stock Keeping Unit)
     categoryId: ObjectId;
     subcategoryId: ObjectId;
-    price: number; 
-    discountedPrice: number; 
+    priceTL: number;
+    discountedPriceTL: number;
     measureUnit: string; // Ölçü birimi (ör. "kg", "lt", "adet")
     measureValue: number; // Ölçü birimi değeri (ör. 1, 2.5)
     stockCount: number;
@@ -17,23 +17,22 @@ export interface IProduct extends Document {
     viewCount: number; // Görüntülenme sayısı
     uniqueLoggedViewCount: number; // Benzersiz kullanıcılar tarafından görüntülenme sayısı
     favouriteCount: number;
-    addedToCartCount: number; 
-    createdBy: ObjectId ;
-    updatedBy: ObjectId ;
+    addedToCartCount: number;
+    createdBy: ObjectId;
+    updatedBy: ObjectId;
     brand: string; // Marka ID
-
 }
 
 const productSchema = new Schema<IProduct>({
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, default: "" },
     image: { type: String, required: true },
     otherImages: { type: [String], default: [] },
     SKU: { type: String, required: true, unique: true },
-    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true  },
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
     subcategoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Subcategory", required: true },
-    price: { type: Number, required: true },
-    discountedPrice: { type: Number, default: 0 },
+    priceTL: { type: Number, required: true },
+    discountedPriceTL: { type: Number, default: null },
     measureUnit: { type: String, required: true },
     measureValue: { type: Number, required: true },
     stockCount: { type: Number, required: true, default: 0 },
@@ -42,13 +41,13 @@ const productSchema = new Schema<IProduct>({
     uniqueLoggedViewCount: { type: Number, default: 0 },
     favouriteCount: { type: Number, default: 0 },
     addedToCartCount: { type: Number, default: 0 },
-    createdBy:{ type: Schema.Types.ObjectId, ref: "User", required: true }, // Kullanıcı ilişkisi için ObjectId kullanılabilir
-    updatedBy:{ type: Schema.Types.ObjectId, ref: "User", required: true }, // Kullanıcı ilişkisi için ObjectId kullanılabilir
-    brand:  { type: String, required: true }, // Marka ID
-}, 
- {
-    timestamps: true, // createDate otomatik eklenecek
-}
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Kullanıcı ilişkisi için ObjectId kullanılabilir
+    updatedBy: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Kullanıcı ilişkisi için ObjectId kullanılabilir
+    brand: { type: String, required: true }, // Marka ID
+},
+    {
+        timestamps: true, // createDate otomatik eklenecek
+    }
 );
 
 const productModel = mongoose.model<IProduct>('Product', productSchema);
